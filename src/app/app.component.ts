cf0595a0d42f65a3523933d44a22a02c1bfe0a62
chenyangfan <chenyangfan3211@gmail.com>
2019-11-20 08:13:58 +0800

    增加swipe库

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Renderer2, ElementRef, ChangeDetectorRef, ViewEncapsulation } from "@angular/core";
+import Swiper from "swiper";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  //修改全局样式
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  
+  aboutTedSwiper: Swiper;
+  public slides = [
+    "First slide",
+    "Second slide",
+    "Third slide",
+  ];
+
+  constructor(private ref: ChangeDetectorRef) {}
+  ngAfterViewInit() {
+    this.aboutTedSwiper = new Swiper(".swiper-container", {
+      // 如果需要分页器
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      centeredSlides : true,
+      loop : true,
+      autoplay:true,
+      // slidesOffsetBefore : 100,
+      slidesOffsetAfter : 100,
+    });
+    this.ref.detectChanges();
+  }
 }
